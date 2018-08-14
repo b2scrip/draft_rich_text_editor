45274707b8aa9b49a1cee67ff65e89954f492124
root <root@ip-172-31-43-68.ap-southeast-1.compute.internal>
2018-08-14 07:18:28 +0000

    delete files not need

--- trans.py
+++
@@ -1,9 +0,0 @@
-#!coding:utf-8
-from googletrans import Translator
-translator = Translator()
-content = '''
-&lt;p&gt;I am following &lt;a href=&quot;https://stevenmiller888.github.io/mind-how-to-build-a-neural-network/&quot; rel=&quot;nofollow noreferrer&quot;&gt;this&lt;/a&gt; article to build 'hello world' in the world of neural networks - XOR.&lt;/p&gt;&#xA;&#xA;&lt;p&gt;During the back-propagation phase author calculates changes in weights between input and hidden layer by calculating &lt;/p&gt;&#xA;&#xA;&lt;p&gt;&lt;a href=&quot;https://i.stack.imgur.com/7BkIL.png&quot; rel=&quot;nofollow noreferrer&quot;&gt;&lt;img src=&quot;https://i.stack.imgur.com/7BkIL.png&quot; alt=&quot;enter image description here&quot;&gt;&lt;/a&gt;&lt;/p&gt;&#xA;&#xA;&lt;p&gt;where&lt;/p&gt;&#xA;&#xA;&lt;pre&gt;&lt;code&gt;w - weights between input and hidden layer&#xA;Hsum - change in the hidden layer sum&#xA;I - input value to  node in NN&#xA;&lt;/code&gt;&lt;/pre&gt;&#xA;&#xA;&lt;p&gt;While this works for input values of 1, it naturally won't work for input values 0. Is it author's mistake and I should look for a better source of learning or I am missing something here?&lt;/p&gt;&#xA;
-''' 
-
-translation = translator.translate(content, dest='zh-cn')
-print(translation.text)
--- tt.py
+++
@@ -1,9 +0,0 @@
-from globe.connect import oauth
-
-oauth = oauth.Oauth("p6zgtLrX4XH9diMynocXAeH4E6prtz9j", "22ed6602d22b70f31405964a96f5290b75a5de708675046176c6673c3ab0d160")
-
-# get redirect url
-print oauth.getRedirectUrl()
-
-# get access token
-print oauth.getAccessToken("G6SLj7j5hBokB5HM4dRKhkBx4GuG6zrqC5KnRgCjMMMzfnonMpFaxKXjsd4Aj6tpGjx9sE4ygnIGnA8rSznXRLhjLrReHkRx5dI5brgnsEj9dgCG5BrgCjpX4XH9diMqq4diXAeH4EBpdCar9zbCBgrzEsynxzxIGErAXHbpXgbhgbAAySo9yREI4gjKzsrxA9Kt8rKrBspdnGxFdzMgGf58ndACrMz7ECAqxKRuALdk4hrbkxkH5B76qhprLbnS")
